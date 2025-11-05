9b84507bd69044d206c5d6b558bd8b3f0c8e9d09
hamza07ejaz <hamza07ejaz@gmail.com>
2025-11-05 02:54:12 -0500

    Update page.tsx

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -10,6 +10,7 @@ export default function PricingPage() {
         "2 Notes per day",
         "1 Q&A per day",
         "Access to Free Study Plan",
+        "Access to Free career help,
       ],
       color: "linear-gradient(90deg,#3aa3ff,#b575ff)",
       border: "rgba(80,120,255,0.5)",
@@ -20,7 +21,7 @@ export default function PricingPage() {
       features: [
         "Unlimited Notes & Q&A",
         "Access to all tools (Flashcards, Test, Visual Map, Presentation, Career Help, Paraphrasing, Grammar, Citations)",
-        "Personalized Study Plans",
+        "Study Plans",
         "Priority Support 24/7",
         "Cancel Anytime",
       ],
