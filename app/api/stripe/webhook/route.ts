51caa1cf2633ab917c5a31354cd0460e332d68ab
hamza07ejaz <hamza07ejaz@gmail.com>
2025-11-05 04:26:15 -0500

    Update route.ts

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -7,10 +7,9 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
 });
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+  process.env.SUPABASE_SERVICE_ROLE_KEY || ""
 );
-
 export async function POST(req) {
   try {
     const sig = req.headers.get("stripe-signature");
